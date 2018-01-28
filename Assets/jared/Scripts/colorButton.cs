09bad72d084661eeb8f1744f21e8d31129f224c6
JAANIS <jared.anders87@gmail.com>
2018-01-28 07:05:58 +0100

    ui panels work
    change lights makes active exct. made prefabs

---
+++ Assets/jared/Scripts/colorButton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class colorButton : MonoBehaviour {
+
+	public Image[] buttonColorsArr = new Image [3];
+	// public Image red;
+	// public Image green;
+	public Image dark;
+	public Image activeOutline;
+
+	public bool is_active;
+	public buttonColor what_color;
+
+
+
+
+	public enum buttonColor{ red, blue, green}
+
+	
+
+	public void changeActiveState(bool state)
+	{
+			activeOutline.enabled = state;
+			dark.enabled = !state;
+	}
+
+
+	public void changeColor(buttonColor newColor)
+	{
+		foreach (var item in buttonColorsArr)
+		{
+			item.enabled = false;
+			
+			if(item.name == newColor.ToString())
+			{
+				item.enabled = true;
+			}
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		changeColor(what_color);
+		changeActiveState(is_active);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			changeActiveState(!activeOutline.enabled);
+		}
+	}
+
+
+
+}
