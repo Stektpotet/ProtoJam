233c430c2816e9dcbed01585ea805c9ae6539622
Arcarnan <nataniel1993@hotmail.com>
2018-01-28 12:48:33 +0100

    added ring rotation

---
+++ Assets/Scripts/Planets/RingRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SpaceCon
+{
+    public class RingRotation : MonoBehaviour
+    {
+        [Range(0.1f, 20f)]
+        public float spin;
+        private void Update()
+        {
+            transform.Rotate(Vector3.down, spin * Time.deltaTime);
+        }
+
+    }
+
+}
