36fefccbc057026d6d2780b9e3f1989b436347ba
JAANIS <jared.anders87@gmail.com>
2018-01-28 09:54:17 +0100

    small fix with add precentagePower

--- Assets/jared/Scripts/PowerBar.cs
+++ Assets/jared/Scripts/PowerBar.cs
@@ -13,33 +13,39 @@ public class PowerBar : MonoBehaviour {
 
 	public float precentagePower = 0.0f;
 	
-	// Use this for initialization
-	void Start () {
-		
-		updatePowerBar();
-		// Debug.Log(tmp);
-		// Debug.Log(mask.Axis);
-		// Debug.Log(RectTransform.Axis.Horizontal.ToString());
 
-		// mask.sizeDelta = new Vector2(0f, 0f);
-		// mask.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, tmp);
-		// Debug.Log()
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKey(KeyCode.UpArrow))
-		{
-			precentagePower += 0.01f;
-			Debug.Log("HAPPNING?");
-			updatePowerBar();
-		}
+	void Start () 
+	{	
+		updatePowerBar();
 	}
 
 
 	private void updatePowerBar()
 	{
-		float tmp = ((numberAtFull - numberAtEmpty) * precentagePower) + numberAtEmpty;
-		mask.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, tmp);
+		float tmpFloat = ((numberAtFull - numberAtEmpty) * precentagePower) + numberAtEmpty;
+		mask.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, tmpFloat);
+	}
+
+
+	void Update()
+	{
+				//FOR DEBUG TEST WITH KEY
+		// if(Input.GetKey(KeyCode.UpArrow))
+		// {
+		// 	setPresentagePower(getPresentagePower() + 0.01f);
+		// }
+	}
+
+
+	public float getPresentagePower()
+	{
+		return precentagePower;
+	}
+
+	public void setPresentagePower(float value)
+	{
+
+		precentagePower = value;
+		updatePowerBar();
 	}
 }
